791d46770def3ed14ab530c298c6abc276eddf76
José María Guisado Gómez <josguigom1@alum.us.es>
2017-04-10 10:36:37 +0200

    First commit.
    Dumped the project.

---
+++ ClassLibrary1/CryptoUtils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoUtils
+{
+    public class CryptoUtils
+    {
+        public static String Sha1StringOf(byte[] input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(input);
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public static String Md5StringOf(byte[] input)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var hash_md5 = md5.ComputeHash(input);
+                var sb = new StringBuilder(hash_md5.Length * 2);
+
+                foreach (byte b in hash_md5)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ Puxhash/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using CryptoUtils;
+using MSM.UI.Practices;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puxhash.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        #region "Private fields"
+        private String _FilePath;
+        //private String _Md5;
+        //private String _Sha1;
+        #endregion
+
+        #region "Properties"
+
+        public String FilePath
+        {
+            get
+            {
+                return _FilePath;
+            }
+            set
+            {
+                _FilePath = value;
+                NotifyPropertyChanged("FilePath");
+                NotifyPropertyChanged("Md5");
+                NotifyPropertyChanged("Sha1");
+            }
+        }
+
+        public String Md5
+        {
+            get
+            {
+                byte[] bytes = File.ReadAllBytes(FilePath);
+                return CryptoUtils.CryptoUtils.Md5StringOf(bytes);
+            }
+        }
+
+        public String Sha1
+        {
+            get
+            {
+                byte[] bytes = File.ReadAllBytes(FilePath);
+                return CryptoUtils.CryptoUtils.Sha1StringOf(bytes);
+            }
+        }
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            FilePath = "input.epub";
+        }
+    }
+}
