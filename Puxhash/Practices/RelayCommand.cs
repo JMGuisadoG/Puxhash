4bcae573501054a7f829faaa243311be353c9493
José María Guisado Gómez <josguigom1@alum.us.es>
2017-04-10 15:58:51 +0200

    Added RelayCommand to the project

---
+++ Puxhash/Practices/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Puxhash.Practices
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        private Action methodToExecute;
+        private Func<bool> canExecuteEvaluator;
+        public RelayCommand(Action methodToExecute, Func<bool> canExecuteEvaluator)
+        {
+            this.methodToExecute = methodToExecute;
+            this.canExecuteEvaluator = canExecuteEvaluator;
+        }
+        public RelayCommand(Action methodToExecute)
+            : this(methodToExecute, null)
+        {
+        }
+        public bool CanExecute(object parameter)
+        {
+            return canExecuteEvaluator == null ? true : canExecuteEvaluator.Invoke();
+            //if (canExecuteEvaluator == null)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    bool result = canExecuteEvaluator.Invoke();
+            //    return result;
+            //}
+        }
+        public void Execute(object parameter)
+        {
+            methodToExecute.Invoke();
+        }
+    }
+}
