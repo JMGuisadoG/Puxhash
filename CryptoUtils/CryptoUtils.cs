bb8db4db412957562c9d02d6dfeaea53f73ebb08
José María Guisado Gómez <jmguisado@iesnervion.es>
2017-09-04 23:24:04 +0200

    Add Md5StringOf doc and move Sha1StringOf up

--- CryptoUtils/CryptoUtils.cs
+++ CryptoUtils/CryptoUtils.cs
@@ -41,6 +41,35 @@ namespace CryptoUtils
         }
 
         /// <summary>
+        ///     Computes the sha1 hash of a file, given its stream, and converts it to a 
+        ///     lowercase hex string
+        /// </summary>
+        /// <param name="fileStream">
+        ///     Stream of the file to be digested
+        /// </param>
+        /// <remarks>
+        ///     To be used with larger size files
+        /// </remarks>
+        /// <returns>
+        ///     The sha1 digest of the input file stream
+        /// </returns>
+        public static string Sha1StringOf(Stream fileStream)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash_sha1 = sha1.ComputeHash(fileStream);
+                var sb = new StringBuilder(hash_sha1.Length * 2);
+
+                foreach (byte b in hash_sha1)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        /// <summary>
         ///     Computes the md5 hash of a given array of bytes and converts it to a 
         ///     lowercase hex string
         /// </summary>
@@ -69,6 +98,20 @@ namespace CryptoUtils
             }
         }
 
+
+        /// <summary>
+        ///     Computes the md5 hash of a file, given its stream, and converts it to a 
+        ///     lowercase hex string
+        /// </summary>
+        /// <param name="fileStream">
+        ///     Stream of the file to be digested
+        /// </param>
+        /// <remarks>
+        ///     To be used with larger size files
+        /// </remarks>
+        /// <returns>
+        ///     The md5 digest of the input file stream
+        /// </returns>
         public static string Md5StringOf(Stream fileStream)
         {
             using (var md5 = MD5.Create())
@@ -77,23 +120,6 @@ namespace CryptoUtils
                 var sb = new StringBuilder(hash_md5.Length * 2);
 
                 foreach (byte b in hash_md5)
-                {
-                    sb.Append(b.ToString("x2"));
-                }
-
-                return sb.ToString();
-            }
-        }
-
-
-        public static string Sha1StringOf(Stream fileStream)
-        {
-            using (SHA1Managed sha1 = new SHA1Managed())
-            {
-                var hash_sha1 = sha1.ComputeHash(fileStream);
-                var sb = new StringBuilder(hash_sha1.Length * 2);
-
-                foreach (byte b in hash_sha1)
                 {
                     sb.Append(b.ToString("x2"));
                 }
